d916f96201d51c004f4abbc48167afde7fa2a910
aelarabawy <aelarabawy.git@lasilka.com>
2016-11-10 22:45:18 -0800

    Updated the functions according to the requirements in the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is the peer graded assignment for W3 in Course 2
+## The following functions perform caching for matrix inverse
+## 
 
-## Write a short comment describing this function
-
+## This function creates the matrix Object
 makeCacheMatrix <- function(x = matrix()) {
 
+    #verify that the matrix is square
+    if(nrow(x) != ncol(x)) {
+        stop("Matrix is not square")
+    }
+    
+    matInverse <-NULL
+
+    set <- function(y) {
+        #verify that the matrix is square
+        if(nrow(y) != ncol(y)) {
+            stop("Matrix is not square")
+        }
+        
+        x <<- y
+        matInverse <<- NULL
+    }
+
+    get <- function() x
+    setInv <- function(inv) matInverse <<- inv
+    getInv <- function() matInverse
+
+    #Now return the list of operation    
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## This function calculates and caches the inverse if needed
+## Or returns the cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inv <- x$getInv()
+    if(!is.null(inv)) {
+        message("getting cached Inverse")
+        return(inv)
+    }
+    
+    message("Calculating the Inverse")
+    mat <- x$get()
+    inv <- solve(mat)
+    x$setInv(inv)
+    inv
 }
